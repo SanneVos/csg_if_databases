f90fd74d965e6fb0579f0ecb3b54564333622aaa
SanneVos <sannevostra@gmail.com>
2020-04-20 09:46:18 +0000

    opdr 3

--- public/PHP/opdracht_3.php
+++ public/PHP/opdracht_3.php
@@ -9,9 +9,15 @@ $waarde=9/10;
 $kwad=pow($waarde,2);
 $rec=1/$kwad;
 $afgerond=round($rec,7);
+$afgekapt=floor(1000000*$rec)/1000000;
 
 echo "Het kwadraat van $waarde is $kwad.<br>
       Het omgekeerde daarvan is (afgerond:) $afgerond<br>";
+
+$a=0.75;
+$b=0.98;
+$c=round(sqrt(pow($a,2)+pow($b,2)),3);
+$mod=fmod($b,$a);
 
 /****************************
 EINDE VAN JOUW PHPCODE

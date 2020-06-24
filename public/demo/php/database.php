eabd7a9f19239910d3b023798a5acd7a1da41372
SanneVos <sannevostra@gmail.com>
2020-06-24 09:02:11 +0000

    Update

--- public/demo/php/database.php
+++ public/demo/php/database.php
@@ -3,7 +3,7 @@ $check = $check.'database.php geladen.<br>';
 $servernaam = "localhost";
 $gebruikersnaam = "username";
 $wachtwoord = "password";
-$database = "project";
+$database = "demo";
 $DBverbinding = mysqli_connect($servernaam, $gebruikersnaam, $wachtwoord, $database);
 
 if (!$DBverbinding) {
---
+++ public/demo/vnr.php
@@ -0,0 +1,9 @@
+<?php
+require('php/header.php');
+?>
+
+<h1>ontwerp van specifieke pagina</h1>
+
+<?php
+require('php/footer.php');
+?>
